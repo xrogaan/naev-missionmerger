adfa41ee72cc074315eba243bb4ae6c4fad98116
Ludovic Bellière <xrogaan@gmail.com>
2010-08-08 23:17:24 +0200

    first big debug

--- main.py
+++ main.py
@@ -20,8 +20,9 @@ class avail:
     chance = None
     cond = None
     done = None
-    location = None
-    faction = {}
+    location = []
+    faction = []
+    planet = []
     
     def __init__(self):
         pass
@@ -36,18 +37,18 @@ class Mission:
 
     def __init__(self, xmlfile):
         self.doc = parse(xmlfile)
-        self.__missionAttribs__["name"] = self.doc.getRootElement().getAttribute('name')
+        self.__missionAttribs__["name"] = self.getRootElement().getAttribute('name')
    
-        self.lua = self.doc.getRootElement().getElementsByTagName('lua')[0].childNodes[0].wholeText
+        self.lua = self.getRootElement().getElementsByTagName('lua')[0].childNodes[0].wholeText
         
         try:
-            self.doc.getRootElement().getElementsByTagName('flags')[0].childNodes[1].tagName
+            self.getRootElement().getElementsByTagName('flags')[0].childNodes[1].tagName
         except:
             self.flags.unique = False
         else:
             self.flags.unique = True
         
-        for availTag in self.doc.getRootElement().getElementsByTagName('avail').childNodes:
+        for availTag in self.getRootElement().getElementsByTagName('avail')[0].childNodes:
             if availTag.nodeType == availTag.TEXT_NODE:
                 continue
             elif availTag.tagName == 'cond':
@@ -59,15 +60,22 @@ class Mission:
             elif availTag.tagName == 'location':
                 locations = [ 'None', 'Computer', 'Bar', 'Outfit', 'Shipyard', 'Land', 'Commodity' ]
                 if availTag.childNodes[0].wholeText not in locations:
-                    sys.stderr.write("Unknow value '%s' for 'location' child of avail tag for mission %s" % (availTag.childNodes[0].wholeText, self.getName()))
+                    sys.stderr.write("Unknow value '%s' for 'location' child of avail tag for mission %s"
+                            % (availTag.childNodes[0].wholeText, self.getName()))
                 else:
-                    self.avail.location[] = availTag.childNodes[0].wholeText
-            elif availTag.tagName == 'action':
-                self.avail.faction = availTag.childNodes[0].wholeText
+                    self.avail.location.append(availTag.childNodes[0].wholeText)
+            elif availTag.tagName == 'faction':
+                self.avail.faction.append(availTag.childNodes[0].wholeText)
             elif availTag.tagName == 'planet':
-                self.avail.planet = availTag.childNodes[0].wholeText
+                self.avail.planet.append(availTag.childNodes[0].wholeText)
             else:
                 sys.stderr.write("Unknow child %s of avail tag for mission %s" % (availTag.tagName, self.getName()))
+
+    def getRootElement(self):
+        if self.__currentNode__ == None:
+            self.__currentNode__ = self.doc.documentElement
+        return self.__currentNode__
+
 
     def getName(self):
         return self.__missionAttribs__["name"]
@@ -80,24 +88,37 @@ class Mission:
 
     def getAvail(self,Node=None):
         if Node != None:
-            return self.avail.Node
+            if hasattr(self.avail, Node):
+                return self.avail.Node
+            else:
+                return None
+        else:
+            return {'chance': self.avail.chance,
+                    'cond': self.avail.cond,
+                    'done': self.avail.done,
+                    'location': self.avail.location,
+                    'planet': self.avail.planet,
+                    'faction': self.avail.faction
+                   }
 
 class TransformXmlToMissions:
-    __missionList__ = None
+    __missionList__ = []
     __verbose__     = True
     
-    def __init__(self):
-        self.readXml()
+    def __init__(self, localpath):
+        self.readXml(localpath)
 
     def readXml(self, localpath):
         localpath = os.path.abspath(os.path.normpath(localpath))
         for root, dirs, files in os.walk(localpath):
             for f in files:
                 filename = os.path.join(root, f)
-                if re.match(".*(\.xml)$", f) and !self.ignore_filename(filename):
+                if self.ignore_filename(filename):
+                    continue
+                if re.match(".*(\.xml)$", f):
                     sys.stdout.write("Processing %s" % (filename))
                     sys.stdout.flush()
-                    self.__missionList__[] = Mission(filename)
+                    self.__missionList__.append(Mission(filename))
         sys.stdout.write("Done")
         sys.stdout.flush()
     
@@ -105,16 +126,22 @@ class TransformXmlToMissions:
         rootxml = ET.Element('Missions')
         for mission in self.__missionList__:
             child = ET.Element('mission', name=mission.getName())
-            child.
+
+
     def tostring(self):
         for mission in self.__missionList__:
-            sys.stdout.write('Name: %s' % (mission.getName())
-            sys.stdout.write('Lua: %s' % (mission.getLua())
+            sys.stdout.write('Name: %s' % mission.getName())
+            sys.stdout.flush()
+            sys.stdout.write('Lua: %s' % mission.getLua())
+            sys.stdout.flush()
             if mission.isUnique() == True:
                 sys.stdout.write('+Is unique')
+                sys.stdout.flush()
             sys.stdout.write('Avail:')
-            for availTag in mission.getAvail():
-                sys.stdout.write('%s: %s' % (availTag['key'], availTag['value'])
+            sys.stdout.flush()
+            for key, val in mission.getAvail().items():
+                sys.stdout.write('%s: %s' % (key, val))
+                sys.stdout.flush()
     
     def ignore_filename(self,filename):
         root, ext = os.path.splitext(filename)
@@ -145,4 +172,5 @@ a big xml.
 
     local_path = args[0]
     local_files = TransformXmlToMissions(local_path)
+    local_files.tostring()
     
